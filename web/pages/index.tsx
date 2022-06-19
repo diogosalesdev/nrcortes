5ac6f368f965402833523d23b5e656642475f471
Diogo Sales <diogosalesdev@gmail.com>
2022-06-19 16:18:30 -0300

    add: the building page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,25 @@
+import { InstagramLogo } from 'phosphor-react';
 
 export default function Home() {
-  return (
-    <h1>NR Cortes</h1>
-  )
+	return (
+		<div className="w-full h-[100vh] bg-white flex flex-col items-center justify-center">
+			<div className="">
+				<img src="logo.svg" alt="Logo do NR Cortes" />
+			</div>
+			<div>
+				<h1 className="text-3xl">
+					<strong> Em construção! </strong>
+				</h1>
+			</div>
+			<div>
+				<a
+					className="p-5 text-xl flex justify-center items-center"
+					href="https://www.instagram.com/nrcortes__/"
+				>
+					<strong className="mr-2"> Siga nosso instagram </strong>
+					<InstagramLogo size={24} />
+				</a>
+			</div>
+		</div>
+	);
 }
